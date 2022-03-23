7a9e04dcb3903a8a5eafe363ba834f7f2adb19d2
Carlos Javier <carlos.riverarivera@protonmail.ch>
2022-03-23 11:09:36 +0000

    Added the command for planaria

--- species_specific_NBarcodeExtractor.sh
+++ species_specific_NBarcodeExtractor.sh
@@ -3,4 +3,5 @@ cut -d"," -f1 Hv_nc_barcodes.csv.tmp > Hv_nc_barcodes.csv
 cut -d"," -f1 Mm_nc_barcodes.csv.tmp > Mm_nc_barcodes.csv
 cut -d"," -f1 Sa_nc_barcodes.csv.tmp > Sa_nc_barcodes.csv
 cut -d"," -f1 Sl_nc_barcodes.csv.tmp > Sl_nc_barcodes.csv
+cut -d"," -f1 Sm_nc_barcodes.csv.tmp > Sm_nc_barcodes.csv
 cut -d"," -f1-3 Xt_nc_barcodes.csv.tmp > Xt_nc_barcodes.csv # Xenopus tropicalis has more barcode information in the other columns, so best to keep a maximum.
