94156d5f37d20511baeb2322320795df2543dee0
Carlos Javier <carlos.riverarivera@protonmail.ch>
2022-03-28 10:22:15 +0100

    geneCountsTabulato.R now uses vectorized opers.

--- geneCountsTabulato.R
+++ geneCountsTabulato.R
@@ -3,61 +3,18 @@ library(data.table)
 # Give a name to your output file
 outFilename<-"<Your output filename>"
 
-# Initialize empty vectors for what will become the 3 columns of the output table:
-namevect<-c() # Gene IDs
-countvect<-c() # in how many cells said gene was ON
-percvect<-c() # in what percent of cells said gene was ON
+namevect<-rownames(NC_cells_NC_genes)
 
-# Iterate through the rows (=genes)...
-for (row in 1:nrow(NC_cells_NC_genes)) {
-    # Inform what's happening - also useful for debugging
-    print(
-        paste(
-            row,
-            "of",
-            nrow(
-                NC_cells_NC_genes
-            )
-        )
-    )
+# Create a boolean version of the table that has TRUE for any value that's nonzero (these tables shouldn't have negative values)
+boolData<-NC_cells_NC_genes != 0
 
-    # Extract the gene's ID: ASSUMES THE GENE IDs are the rownames - WILL NOT WORK IF THE GENE IDs are listed in a column.
-    gene_ID<-rownames(
-        NC_cells_NC_genes[row,]
-        )
-    # Count how often the gene is expressed in its row (how often the value in the row is nonzero).
-    count<-length(
-            which(
-                NC_cells_NC_genes[row,] != 0
-                )
-            )
-    # Calculate the percent that represents.
-    percent<-(
-        count/ncol(
-          NC_cells_NC_genes
-        )
-    )*100
+# because TRUE is also a 1, we can add the totals of each row, and get the counts of how many TRUE values each row had.
+countvect<-rowSums(boolData)
 
-    # Add the current gene ID to the gene ID vector.
-    namevect<-c(
-        namevect,
-        gene_ID
-    )
+# to get the percentage, just divide by the total number of cells
+percvect<-100*(countvect/ncol(NC_cells_NC_genes))
 
-    # Add the current counts to the counts vector.
-    countvect<-c(
-        countvect,
-        count
-    )
-
-    # Add the current percent to the percents vector.
-    percvect<-c(
-        percvect,
-        percent
-    )
-} #Iteration finishes
-
-# Create a new dataframe that has the vectors created in the loop above as the columns
+# Create a new dataframe that has the vectors created above as columns.
 outDF<-data.frame(
     gene.ID=namevect,
     numCellsOn=countvect,
