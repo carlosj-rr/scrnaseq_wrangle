29f49f8ac5dc6b595b5a4e701e247c36d1037f2f
Carlos Javier <carlos.riverarivera@protonmail.ch>
2022-03-30 09:58:35 +0100

    Updating guide

--- ExprTable_subsampler.r
+++ ExprTable_subsampler.r
@@ -8,7 +8,12 @@ inData<-fread(
 # Confirm the column names correspond to the barcodes out lists. They should, but otherwise make it so (there are species-specific notes where some modifications were needed).
 colnames(
     inData
-    ) 
+    )
+
+# Confirm the row names correspond to the gene IDs. They should, but some species need a bit of formatting here, see under species specific notes.
+rownames(
+    inData
+)
 
 # Read in the "[speciescode]_nc_barcodes.csv" tables you produced earlier
 nc_barcodes<-read.table(
@@ -26,18 +31,11 @@ nc_column_idcs<-which(
     colnames(inData) %in% nc_barcodes$barcode
     ) 
 
-# Extract those columns only from the whole scRNA-seq table
-NC_cells<-inData[,..nc_column_idcs] 
+# Extract those columns only from the whole scRNA-seq table, and re-set the row names to the gene IDs
+NC_cells<-inData[,..nc_column_idcs]
+rownames(NC_cells)<-rownames(inData)
 
-# OPTIONAL: Add the gene names as row names to your table. Usually these will be either as rownames already in "inData", or as the first column name.
-# for example, although often it needed a bit more wrangling than that, especially if the gene IDs had a description attached.
-gene_names<-inData[,1] 
-rownames(
-    # Because NC_cells has the same number and order of rows as inData, we can assign the row numbers like this.
-    NC_cells
-)<-gene_names 
-
-# FOR THE SPONGE - SEE BELOW
+##### FOR THE SPONGE - SEE BELOW #####
 # IF the gene name has a description attached, it will probably have the ID, then a space, then a description. In these cases, do:
 short_names<-c() #inintialize a vector to store all the ids withOUT their description
 for (idx in 1:length(gene_names))  {
@@ -58,6 +56,9 @@ rownames(
     NC_cells
 )<-short_names
 
+##### DONE WITH SPONGE-SPECIFIC STUFF#####
+
+
 ### Remove genes that are NOT expressed in neural cells (i.e., that their expression value is '0' through all neural cells)
 row_tots<-rowSums(
     NC_cells
@@ -67,9 +68,11 @@ genes_on_idcs<-which(
     row_tots > 0
 ) # get row number of genes that have counts/expression above 0.
 
+# Subsample the rows, and re-set the rownames again
 NC_cells_NC_genes<-NC_cells[genes_on_idcs,]
+rownames(NC_cells_NC_genes)<-rownames(NC_cells)[genes_on_idcs]
 
-# Save table to file!
+# Save table to file! (confirm the following things before: 1. row names are gene IDs, 2. column names are cell barcodes, and 3. output file is CSV)
 write.table(NC_cells_NC_genes,
     "<yourfilenamehere>",
     sep=",",
@@ -78,5 +81,152 @@ write.table(NC_cells_NC_genes,
     row.names=T
 ) # Add any additional formatting flags
 # This table will allow you to do two main things:
-    # 1: Make the pan neural cell transcriptome by getting the matching sequences from the reference transcriptomes
-    # 2. Tabulate on how many cells each gene is expressed - "geneCountsTabulato.R".
+    # 1: Make the pan-neural cell transcriptome by getting the matching sequences from the reference transcriptomes
+    # 2. Tabulate on how many cells each gene is expressed
+
+# Tabulate the on how many cells each gene is expressed:
+
+namevect<-rownames(NC_cells_NC_genes) # Hydra's command is different
+
+### MODIFICATION FOR HYDRA BELOW ###
+splitter<-function(longname) {
+    shortname<-strsplit(longname,split="\\|")[[1]][1];
+    return(shortname)
+    }
+
+outDF$gene.ID<-as.vector(sapply(outDF$gene.ID,splitter))
+namevect<-outDF$gene.ID
+### END OF MODIFICATION FOR HYDRA ###
+
+# Create a boolean version of the table that has TRUE for any value that's nonzero (these tables shouldn't have negative values)
+boolData<-NC_cells_NC_genes != 0
+
+# because TRUE is also a 1, we can add the totals of each row, and get the counts of how many TRUE values each row had.
+countvect<-rowSums(boolData)
+
+# to get the percentage, just divide by the total number of cells
+percvect<-100*(countvect/ncol(NC_cells_NC_genes))
+
+# Create a new dataframe that has the vectors created above as columns.
+outDF<-data.frame(
+    gene.ID=namevect,
+    numCellsOn=countvect,
+    percentCellsOn=percvect
+)
+
+# Sort data frame by decreasing percent (most common genes at the top), and select only the genes which are expressed in 50% of cells or more
+outDF<-outDF[order(-outDF$percentCellsOn),]
+outDF<-outDF[outDF$percentCellsOn >= 50,]
+
+# Save table to disk
+write.table(outDF,
+    "outFile.csv",
+    sep=",",
+    quote=F,
+    row.names=F
+)
+
+# Saving neural cell-only transcriptome
+
+library(ape)
+
+Trome<-read.FASTA("<Your reference transcriptome file>")
+tromeLabels<-labels(Trome) # Make a vector with only the labels of the transcriptome, so that a simpler data structure can be queried later.
+
+# Define a function that extracts the length of a sequence, given an index number, and a reference transcriptome
+seq_len<-function(idx,Trome) {
+    size<-as.integer(
+        summary(
+            Trome[idx]
+                )[1]
+            )
+    return(
+        size
+        )
+}
+
+# Define a function that uses 'seq_len()' to get, from a vector of sequence indices in a transcriptome, the longest sequence. This is useful for cases in which the same gene ID has several transcripts, and you want only the longest - canonical one.
+getLongestSeqIdx<-function(inp_vect) {
+            topsize<-0
+            for (i in inp_vect) {
+                    len<-seq_len(
+                        i,
+                        Trome
+                        )
+                if (len > topsize) {
+                        topsize<-len
+                        outidx<-i
+                    }
+                }
+    
+    return(outidx)
+}
+
+
+# From a list of gene names, get the index of the longest transcript present in the reference transcriptome. Also output a table that lists all the IDs that were not found.
+geneIdxFinder<-function(geneids) {
+        idcs_vect<-c()
+        orphans<-c()
+            for (
+                id in geneids
+                )
+                {
+                    print(paste("Getting the sequence index of",id))
+                    # 'grep' can be tricky sometimes, because many names can be subsets of other names, this is why a query input must be built with paste.
+                    # In this case, it will look for the sequence ID, and en EOL character afterwards.
+                    query<-paste(id,"$",sep="")
+                    tidx<-grep(query,tromeLabels)
+        
+                    if (
+                        length(
+                            tidx) == 1 
+                            )
+                            {
+                                idcs_vect<-c(
+                                    idcs_vect,
+                                    tidx
+                                    )
+                            }
+                            else if (length(tidx) > 1)
+                            {
+                                print(tidx)
+                                tidx<-getLongestSeqIdx(
+                                    tidx
+                                    )
+                                idcs_vect<-c(
+                                    idcs_vect,
+                                    tidx
+                                    )
+                            } else if (length(tidx) == 0) {
+                                print(paste(id,"not found"))
+                                orphans<-c(orphans,id)
+                            }
+                }
+                    idcs_vect<-sort(idcs_vect)
+    return(list(idcs_vect,orphans))
+}
+
+# Get the transcriptome indices of all the genes in the table. Because the table is a neural cell- and neural gene-only subselection, we can fetch them all to produce a neural transcriptome.
+x<-geneIdxFinder(
+    outDF$gene.ID
+    )
+tromeSubsampleIdcs<-x[[1]]
+orphans<-x[[2]]
+
+# Make the neural cell transcriptome by subsampling the reference transcriptome, store orphan hits, if any, and if so, also remove them from the 'outDF' table.
+if (length(orphans) == 0 ){
+    NC_Trome<-Trome[tromeSubsampleIdcs]
+} else {
+    NC_Trome<-Trome[tromeSubsampleIdcs]
+    print("There is a nonzero amount of orphans, please see list in the file <orphans.csv>")
+    write.table(orphans,"orphans.csv",sep=",",quote=F,col.names=F,row.names=F)
+    print("In addition, these IDs will be removed from the <outDF> table with the most common genes in NC cells")
+    outDF<-outDF[which(!outDF$gene.ID %in% orphans),]
+    print("outDF table of NC gene counts has been updated - I kindly suggest that the table is re-saved to disk (see 'write.table()' command above)")
+}
+
+# Save it as a FASTA file
+write.FASTA(
+    NC_Trome,
+    "<filename.fas>"
+)
--- Hv_notes.md
+++ Hv_notes.md
@@ -1,27 +1,9 @@
-#### __Hydra vulgaris__-specific hacks
-In _Hydra_'s gene expression tables, the barcodes are prepended by an X, like so: "X01.D1_ATGCATACACTA" in the gene expression table is written as "01-D1_ATGCATACACTA" in the "Hv_nc_barcodes.csv" file. Notice also that the period (".") in between the first pair of numbers arnd the barcode is swapped for a hyphen ("-") in the lookup table. This can be corrected with bash, but also with R, once the gene expression table was imported.
-Ex:
+#### _Hydra vulgaris_-specific hacks
+In _Hydra_'s gene expression tables, a simple ```fread("Hv_full_expressionTable.csv")``` will import the gene names as the first column, and fill the rownames with numbers. Because 'fread' doesn't implement an option to specify row names from a column's values, we need to do this 'by hand':
 ````
-x<-"X01.D1_ATGCATACACTA"
-> sub("\\.","-",sub("X","",x)) # This nested command first removes the "X", and then swaps the period "." for a hyphen "-". The period character has to be escaped because otherwise it means "any character".
-[1] "01-D1_ATGCATACACTA"
-````
-Ideally though, we would run this command for every single column name, then re-set the column names with the resulting (corrected) vector of column names. This function can be used as a vectorized operation, and we can change all the column names in one full sweep.
-
-````
-col_name_cleaner<-function(incolname) {
-    corr<-sub("\\.","-",sub("X","",incolname))
-    return(corr)
-}
-to_be_fixed<-colnames(inData)
-fixed<-col_name_cleaner(to_be_fixed)
-colnames(inData)<-fixed
+inData<-fread("Hv_full_expressionTable.csv")
+rownames(inData)<-inData$V1
 ````
 You can run ```colnames(inData)``` to confirm that the results are what you want.
 
-11 mitochondrial genes come up as genes expressed in neural cells, and are not in the reference transcriptome. However, they're expressed in a negligible amount of cells (0.02%-0.33% of all cells), so I'll disregard these. Later on, I will remove the genes expressed in less than 50% of all cells.
-
-````
-last_nuc_gene_idx<-which(rownames(NC_cells_NC_genes) == "MT_ATP8")-1 # MT_ATP8 is the first mitochondrial gene of the list, and they're the last 11 row names, so the index before that should be the last gene we get from the reference transcriptome
-tromeSubsampleIdcs<-geneIdxFinder(rownames(NC_cells_NC_genes)[1:last_nuc_idx]) # geneIdxFinder()'s call can be modified.
-````
+When creating the 'outDF' table, the command that produces the 'namevect' has to be modified because the expression data table has a bit of annotation for each gene. The necessary changes are detailed in the "ExprTable_subsampler.R" script.
