6447b68a2b2f235800ffe87eec47214c823a91fd
carlosj-rr <carlos.riverarivera@bristol.ac.uk>
2023-11-10 10:21:11 +0000

    updated python parser

--- scrnaseq_parser.py
+++ scrnaseq_parser.py
@@ -4,19 +4,7 @@ import scipy as sc
 import anndata as ad
 import numpy as np
 
-def col_matcher(query,reference):
-    for i in range(0,len(reference)):
-        if query in reference[i]:
-            ans=i
-    return(ans)
-
-def percent(counts_array: int,num_cells: int) -> float:
-    return(100*(counts_array/num_cells))
-
-def col_nc_representation(column: pd.core.series.Series, nc_idcs: list) -> np.ndarray:
-    col_ON=np.where(column > 0)
-    return(np.intersect1d(col_ON,nc_idcs))
-
+#needed
 def check_inputs(ad_filename: str, ncgenes_filename: str):
     if ".loom" in ad_filename:
         loom=True
@@ -36,7 +24,8 @@ def check_inputs(ad_filename: str, ncgenes_filename: str):
     else:
         raise TypeError(f"List of nerve cell gene markers in an unknown format.\n\nPlease make sure it is in either '.csv' or '.tsv. format.")
 
-def data_importer(ad_filename: str, ncgenes_filename: str) -> (ad._core.anndata.AnnData, np.ndarray):
+#needed
+def data_importer(ad_filename: str, ncgenes_filename: str):
     if ".loom" in ad_filename:
         ds_ad=sp.read_loom(ad_filename)
     elif ".h5ad" in ad_filename:
@@ -47,6 +36,7 @@ def data_importer(ad_filename: str, ncgenes_filename: str) -> (ad._core.anndata.
         nc_markers=np.array(pd.read_csv(ncgenes_filename, sep="\t")).flatten()
     return(ds_ad,nc_markers)
 
+#needed
 def counts_table_extractor(ad_obj: ad._core.anndata.AnnData) -> pd.DataFrame:
     if type(ad_obj.X) == sc.sparse._csr.csr_matrix:
         out_tab=ad_obj.X.todense()
@@ -57,44 +47,41 @@ def counts_table_extractor(ad_obj: ad._core.anndata.AnnData) -> pd.DataFrame:
     out_tab=pd.DataFrame(out_tab,columns=colnames,index=rownames)
     return(out_tab)
 
-def count_nc_genes(ad_filename: str, ncgenes_filename: str) -> pd.DataFrame:
+#OF number 1
+def count_nc_genes(ad_filename: str, ncgenes_filename: str) -> (pd.Series,pd.DataFrame):
     # check if anndata object is in .loom or in .h5ad format:
     check_inputs(ad_filename, ncgenes_filename)
 
     # import the input data:
     ds_ad,nc_markers=data_importer(ad_filename, ncgenes_filename) #function ensures right datatype output.
 
-    # extract the tables - make sure the output table is a dense matrix, turn it into a pandas DF with the correct column and row names.
-    ds_data=counts_table_extractor(ds_ad)
+    #Check whether the imported table is an anndata object or not if it is, run 'counts_table_extractor'
+    if type(ds_ad) == pd.DataFrame:
+        ds_data=ds_ad
+    elif type(ds_ad) == ad.AnnData:
+        # extract the tables - make sure the output table is a dense matrix, turn it into a pandas DF with the correct column and row names.
+        ds_data=counts_table_extractor(ds_ad)
+    
 
-    l=[]
-    for k in nc_markers:
-        l.append(col_matcher(k,ds_data.columns))
-    l.sort()
-
-    list1=[]
-    tot_cells=len(ds_data.columns)
-    for col in range(0,tot_cells):
-        val=len(col_nc_representation(ds_data.iloc[:,col],l))
-        list1.append(val)
-        print(f"Finished column {col} of {tot_cells}")
-       
-    gt0=pd.DataFrame(list(zip(ds_data.columns,list1)),columns=("barcode","counts"))
+    nc_gene_ss=ds_data.loc[nc_markers,:]
+    gt0=np.sum((nc_gene_ss > 0).astype(int),axis=0)
     
     return(gt0,ds_data)
-
-def nc_gene_set_and_counts(cell_by_nc_marker_counts: pd.DataFrame, ds_data: pd.DataFrame) -> pd.DataFrame:
-    gt0=cell_by_nc_marker_counts
-    unique, count=np.unique(gt0["counts"], return_counts=True)
-    max_num_ncmarkers=unique[-1]
-    num_cells_max_exp=count[-1]
-    nc_cells=np.array(gt0["barcode"][np.where(gt0["counts"] == max_num_ncmarkers)[0]])
+#OF 2
+def top_tenperc_nc_gene_ids_extractor(nc_markers_representation: pd.Series,ds_data: pd.DataFrame, outfile: str) -> None:
+    unique3, counts3 = np.unique(nc_markers_representation,return_counts=True)
+    tot_cells=nc_markers_representation.shape[0]
+    ten_perc=int(np.round(tot_cells*0.1))
+    inverted_cumsum=np.cumsum(np.flip(counts3))
+    smallest_diff_idx=np.where(abs(inverted_cumsum-ten_perc) == abs(inverted_cumsum-ten_perc).min())[0]
+    ten_perc_nc_cell_threshold=np.flip(unique3)[smallest_diff_idx][0]
+    nc_occupancy=unique3[np.where(unique3 == ten_perc_nc_cell_threshold)[0][0]:len(unique3)]
     out_arr=np.array([],dtype=str)
-    for barcode in nc_cells:
-        genes_on_in_cell=ds_data.columns[np.where(ds_data.loc[[barcode]] > 0)[1]]
-        out_arr=np.append(out_arr,genes_on_in_cell)
+    for occup in nc_occupancy:
+        new=np.array(nc_markers_representation.index[np.where(nc_markers_representation == occup)[0]])
+        out_arr=np.append(out_arr,new)
 
-    unique,counts=np.unique(out_arr,return_counts=True)
-    perc_occup=percent(counts,num_cells_max_exp)
-    out_df=pd.DataFrame(list(zip(unique,counts,perc_occup)),columns=["ID","Counts","Percent"])
-    return(out_df)
+    ten_perc_nc_cells_df=ds_data.loc[:,out_arr]
+    nc_genes=ten_perc_nc_cells_df.index[np.where(ten_perc_nc_cells_df.sum(axis=1) > 0)[0]]
+    np.savetxt(outfile,nc_genes,delimiter=",",fmt='%s')
+    return
